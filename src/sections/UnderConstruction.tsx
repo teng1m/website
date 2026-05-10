0ef2ab596f68372143076af54ba7162c06808882
Teng Ma <katoryx@gmail.com>
2026-05-10 01:42:20 -0400

    add under construction page

---
+++ src/sections/UnderConstruction.tsx
@@ -0,0 +1,9 @@
+export default function UnderConstruction() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full gap-3 glass-borders">
+            <img src="/warning.ico" alt="warning" className="size-16" />
+            <h1>Under construction!</h1>
+            <p className="text-sm">Check back soon.</p>
+        </div>
+    );
+}
